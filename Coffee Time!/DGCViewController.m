b2fed7c92bbc264833a4f58c5846ced817f1893e
Ricky Chang <rchang1@gmail.com>
2014-03-12 14:02:16 -0400

    Initial storyboard with UI components

--- Coffee Time!/DGCViewController.m
+++ Coffee Time!/DGCViewController.m
@@ -10,6 +10,12 @@
 
 @interface DGCViewController ()
 
+@property (nonatomic, strong) IBOutlet UILabel *label;
+
+@property (nonatomic, strong) IBOutlet UISlider *slider;
+
+@property (nonatomic, strong) IBOutlet UIProgressView *progressView;
+
 @end
 
 @implementation DGCViewController
@@ -17,13 +23,36 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    
+    NSLog(@"View is loaded.");
+    
+    self.view.backgroundColor = [UIColor grayColor];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+-(IBAction)buttonWasPressed:(id)sender
+{
+    NSLog(@"Button was pressed.");
+    
+    // Get current date and time
+    NSDate *date = [NSDate date];
+    
+    // Update label.
+    self.label.text = [NSString stringWithFormat:@"Button pressed at %@", date];
+}
+
+-(IBAction)sliderValueChanged:(id)sender
+{
+    NSLog(@"Slider value changed to %f", self.slider.value);
+    
+    // Update our progressView's progress to match
+    // the slider value.
+    self.progressView.progress = self.slider.value;
 }
 
 @end
